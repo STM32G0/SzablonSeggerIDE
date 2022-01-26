91e87dfb0d5b973df122de5f6e2d9d1e548724bd
STM32G0 <stm32g0@gmail.com>
2022-01-26 12:32:43 +0100

    extension system_config.h

--- Projekt_Szablon_STM32G0/system_config.h
+++ Projekt_Szablon_STM32G0/system_config.h
@@ -8,6 +8,14 @@ IDE   : SEGGER Embedded Studio
 #ifndef SYSTEM_CONFIG_H
 #define  SYSTEM_CONFIG_H
 
+#include "clock_config.h"
+#include "pin_config.h"
+#include "system_config.h"
+#include "i2c_config.h"
+#include "spi_config.h"
+#include "interrupt_config.h"
+#include "timer_config.h"
+
 void SYSTEM_MANAGER_Initialize(void);
 
 #endif /*SYSTEM_CONFIG_H*/
